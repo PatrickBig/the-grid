bfe61a89c4f263c3eda599add35b6949e7dfbd1c
Patrick Bigler <patrick@biglernet.com>
2023-02-12 10:50:13 -0600

    Starting query runner framework

---
+++ TheGrid.QueryRunners/QueryResults.cs
@@ -0,0 +1,9 @@
+﻿namespace TheGrid.QueryRunners
+{
+    public class QueryResults
+    {
+        public IEnumerable<string> Columns { get; set; } = Enumerable.Empty<string>();
+
+        public IEnumerable<Dictionary<string, object>>  Results { get; set; } = Enumerable.Empty<Dictionary<string, object>>();
+    }
+}
