e86b5cf24806632e586e6cff702f64e5273ed9cd
Patrick Bigler <patrick@biglernet.com>
2023-08-19 09:28:19 -0500

    Removed unused ace editor

---
+++ source/TheGrid.Models/Visualizations/ChartVisualization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGrid.Models.Visualizations
+{
+    public enum ChartType
+    {
+        Area,
+
+        Bar,
+
+        Pie,
+
+        Line,
+    }
+    public class ChartVisualization : VisualizationBase
+    {
+        public ChartType Type { get; set; }
+    }
+}
---
+++ source/TheGrid.Models/Visualizations/VisualizationBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGrid.Models.Visualizations
+{
+    public enum VisualizationType
+    {
+        Table,
+        Chart,
+        Gauge,
+        Counter,
+    }
+
+    public abstract class VisualizationBase
+    {
+        /// <summary>
+        /// Unique identifier for the vizualization.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Reference to the query for the visualization.
+        /// </summary>
+        public int QueryId { get; set; }
+
+        /// <summary>
+        /// Navigation property to the query the visualization is tied to.
+        /// </summary>
+        public Query? Query { get; set; }
+
+        /// <summary>
+        /// Name of the visualization.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+    }
+}
