291ae25e0b74ffaf1daf96f6f2df49db0f0038b4
Patrick Bigler <18059535+PatrickBig@users.noreply.github.com>
2024-05-11 21:49:36 -0500

    Added utility to safely set the name of elements Added tests for the ConnectionPropertyEditor component

---
+++ source/TheGrid.Client/Utilities/HtmlUtility.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace TheGrid.Client.Utilities
+{
+    public static class HtmlUtility
+    {
+        public static string GetSafeId(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return string.Empty;
+            }
+
+            input = input.ToLowerInvariant();
+
+            var sb = new StringBuilder(input.Length);
+            var startIndex = 0;
+
+            if (!char.IsLetter(input[0]))
+            {
+                if (char.IsLetter(input[1]))
+                {
+                    startIndex = 1;
+                }
+                else
+                {
+                    startIndex = 2;
+                }
+            }
+
+            for (var i = startIndex; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                if (char.IsLetter(c) || char.IsNumber(c) || c == '-' || c == '_')
+                {
+                    sb.Append(c);
+                }
+                else if (char.IsWhiteSpace(c))
+                {
+                    sb.Append('-');
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
